11230069cd28c6a83361b50d5524269d0f958deb
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-04-10 18:52:31 -0400

    Basic landing page details and arrow button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style type="text/css">
+        {`
+.btn-flat {
+  background-color: white;
+  color: white;
+  border: white;
+}
+
+.btn-xxl {
+  padding: 1rem 1.5rem;
+  font-size: 1.5rem;
+}
+`}
+      </style>
+      <div className="App">
+        <Button className = "homepage-btn" variant="flat" size="xxl">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp9AxhxlZhyxlUC9Uw6MEE97d642ROWXu28Hadm_5t&s"></img>
+        </Button>
+        <h2 className= "homepage-header">Hi, <br />
+          I'm Grace, <br />
+          Full Stack Software Developer, <br />
+          and Poet</h2>
+      </div>
+    </>
   );
 }
 
 export default App;
+
