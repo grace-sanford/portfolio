085b6187a0a6632b60508976a9fa3132430a3bfd
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-04-12 11:32:51 -0400

    Edits to About me

--- src/aboutMe/AboutMe.jsx
+++ src/aboutMe/AboutMe.jsx
@@ -4,10 +4,10 @@ const AboutMe = () => {
   return (
     <div className="text">
       <p>
-        Grace Sanford aims to bring analytic, inclusive, and creative
-        thinking to software development. Grace recieved her training in the
-        Grace Hopper web development immersive program, where she invested over
-        553 hours on full stack JavaScript development, including{" "}
+        Hi! I'm Grace (she/her/hers). I bring analytic, inclusive, and creative
+        thinking to software development. I recieved my training in the Grace
+        Hopper web development immersive program, where I spent over 553 hours
+        on full stack JavaScript development, including{" "}
         <span className="green">React</span> and{" "}
         <span className="green">Redux</span>,{" "}
         <span className="green">Node</span>,{" "}
@@ -16,16 +16,16 @@ const AboutMe = () => {
         <span className="blue">Postgres</span> databases.
       </p>
       <p>
-        Professionally, Grace is interested in creating{" "}
+        Professionally, I am interested in creating{" "}
         <span className="pink">sustainable</span> and{" "}
-        <span className="pink">financially optimal solutions</span>, by
-        building <span className="pink">technology that helps people</span>.
-        Her experience is in fast-paced, client-facing roles, as a Marketing
+        <span className="pink">financially optimal solutions</span>, by building{" "}
+        <span className="pink">technology that helps people</span>. My
+        experience is in fast-paced, client-facing roles, as a Marketing
         Strategist for a gut-health podcast, as an Assistant Publicist for
         business and non-fiction authors, and most recently as a reservationist
-        for an upscale bar and restaurant. Grace holds a B.A. in Sociology, with
-        a cerficate in Social, Cultural, and Critical Theory from Wesleyan
-        University. Grace lives in New York City.
+        for an upscale bar and restaurant. I hold a B.A. in Sociology, with a
+        cerficate in Social, Cultural, and Critical Theory from Wesleyan
+        University. I live in New York City.
       </p>
     </div>
   );
