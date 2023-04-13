e313e18c6594dce8f39e7d49317acf8071ba73a6
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-04-12 22:08:59 -0400

    Corrected font size

--- src/nav/MyNavDropdown.jsx
+++ src/nav/MyNavDropdown.jsx
@@ -5,59 +5,66 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 
 const MyNavDropdown = () => {
-
   return (
-    <Nav className="nav flex-column" variant="pills" activeKey={null}>
-      <NavDropdown title="Software Projects" id="nav-dropdown">
-        {/**Gallery House */}
-        <LinkContainer to="/galleryhouse">
-          <NavDropdown.Item eventKey="1.1" title="Item">
-            Gallery House
+    <div className="App">
+      <Nav className="nav flex-column" fill variant="pills" activeKey={null}>
+        <NavDropdown title="Software Projects" id="nav-dropdown">
+          {/**Gallery House */}
+          <LinkContainer to="/galleryhouse">
+            <NavDropdown.Item
+              style={{ width: "5rem" }}
+              eventKey="1.1"
+              title="Item"
+              id="nav-dropdown-item"
+            >
+              Gallery House
+            </NavDropdown.Item>
+          </LinkContainer>
+
+          {/**Git-clothes */}
+          <LinkContainer to="/gitclothes">
+            <NavDropdown.Item eventKey="1.2" title="Item" id="nav-dropdown-item">
+              Git-clothes
+            </NavDropdown.Item>
+          </LinkContainer>
+          <NavDropdown.Divider />
+
+          {/**My GitHub */}
+          <NavDropdown.Item
+            eventKey="1.3"
+            // onClick={() => alert(`Redirecting you to GitHub. Okay?`)}
+            href="https://github.com/grace-sanford"
+            target="_blank"
+            id="nav-dropdown-item"
+          >
+            My GitHub
           </NavDropdown.Item>
-        </LinkContainer>
+        </NavDropdown>
 
-        {/**Git-clothes */}
-        <LinkContainer to="/gitclothes">
-          <NavDropdown.Item eventKey="1.2" title="Item">
-            Git-clothes
-          </NavDropdown.Item>
-        </LinkContainer>
-        <NavDropdown.Divider />
+        <Nav.Item>
+          {/**About */}
+          <LinkContainer to="/about">
+            <Nav.Link eventKey="2" title="Item" id="nav-item">
+              About me
+            </Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
 
-        {/**My GitHub */}
-        <NavDropdown.Item
-          eventKey="1.3"
-          // onClick={() => alert(`Redirecting you to GitHub. Okay?`)}
-          href="https://github.com/grace-sanford"
-          target="_blank"
-        >
-          My GitHub
-        </NavDropdown.Item>
-      </NavDropdown>
+        {/**Links */}
+        <Nav.Item>
+          <LinkContainer to="/links">
+            <Nav.Link eventKey="3" id="nav-item">Links</Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
 
-      <Nav.Item>
-        {/**About */}
-        <LinkContainer to="/about">
-          <Nav.Link eventKey="2" title="Item">
-            About me
-          </Nav.Link>
-        </LinkContainer>
-      </Nav.Item>
-
-      {/**Links */}
-      <Nav.Item>
-        <LinkContainer to="/links">
-          <Nav.Link eventKey="3">Links</Nav.Link>
-        </LinkContainer>
-      </Nav.Item>
-
-      {/**Contact me */}
-      <Nav.Item>
-        <LinkContainer to="/contact">
-          <Nav.Link eventKey="4">Get in touch</Nav.Link>
-        </LinkContainer>
-      </Nav.Item>
-    </Nav>
+        {/**Contact me */}
+        <Nav.Item>
+          <LinkContainer to="/contact">
+            <Nav.Link eventKey="4" id="nav-item">Get in touch</Nav.Link>
+          </LinkContainer>
+        </Nav.Item>
+      </Nav>
+    </div>
   );
 };
 
