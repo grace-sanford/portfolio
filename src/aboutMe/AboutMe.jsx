11586098ceec789bc6e5165c4380f49e0fcd9b8a
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-05-14 16:40:18 -0400

    About Me component scroll

--- src/aboutMe/AboutMe.jsx
+++ src/aboutMe/AboutMe.jsx
@@ -1,38 +1,44 @@
 import "../App.css";
+import Container from "react-bootstrap/Container";
 
 const AboutMe = () => {
   return (
-    <div className="App">
-      <div className="text">
-        <p>
-          Grace Sanford brings analytic, inclusive, and creative thinking
-          to software development. Grace recieved her training in the Grace
-          Hopper web development immersive program, where she spent over 553
-          hours on full stack JavaScript development, including{" "}
-          <span className="yellowgreen">React</span> and{" "}
-          <span className="yellowgreen">Redux</span>,{" "}
-          <span className="yellowgreen">Node</span>,{" "}
-          <span className="lightblue">Express</span>, and{" "}
-          <span className="lightblue">Sequelize</span> object relational mapping for{" "}
-          <span className="lightblue">Postgres</span> databases.
-        </p>
-        <p>
-          Professionally, Grace is interested in creating{" "}
-          <span className="pink">accessible</span>,
-          <span className="pink">performant</span>, and{" "}
-          <span className="pink">financially optimal</span> solutions by
-          building <span className="pink">technology that helps people</span>.
-          Grace has experience in fast-paced, client-facing roles, as a
-          Marketing Strategist for a gut-health podcast, an Assistant Publicist
-          for business and non-fiction authors, and most recently a
-          reservationist for an upscale bar and restaurant, where she designed
-          and implemented a single system for tracking 1000+ clients accross 3
-          platforms and 4 businesses. Grace holds a B.A. in Sociology, with a
-          cerficate in Social, Cultural, and Critical Theory from Wesleyan
-          University. Grace lives in New York City.
-        </p>
+    // <div className="App">
+      <div className="section" id="about">
+        <Container>
+          <div className="text">
+            <p>
+              Grace Sanford brings analytic, inclusive, and creative thinking to
+              software development. Grace recieved her training in the Grace
+              Hopper web development immersive program, where she spent over 553
+              hours on full stack JavaScript development, including{" "}
+              <span className="yellowgreen">React</span> and{" "}
+              <span className="yellowgreen">Redux</span>,{" "}
+              <span className="yellowgreen">Node</span>,{" "}
+              <span className="lightblue">Express</span>, and{" "}
+              <span className="lightblue">Sequelize</span> object relational
+              mapping for <span className="lightblue">Postgres</span> databases.
+            </p>
+            <p>
+              Professionally, Grace is interested in creating{" "}
+              <span className="pink">accessible</span>,
+              <span className="pink">performant</span>, and{" "}
+              <span className="pink">financially optimal</span> solutions by
+              building{" "}
+              <span className="pink">technology that helps people</span>. Grace
+              has experience in fast-paced, client-facing roles, as a Marketing
+              Strategist for a gut-health podcast, an Assistant Publicist for
+              business and non-fiction authors, and most recently a
+              reservationist for an upscale bar and restaurant, where she
+              designed and implemented a single system for tracking 1000+
+              clients accross 3 platforms and 4 businesses. Grace holds a B.A.
+              in Sociology, with a cerficate in Social, Cultural, and Critical
+              Theory from Wesleyan University. Grace lives in New York City.
+            </p>
+          </div>
+        </Container>
       </div>
-    </div>
+    // </div>
   );
 };
 
--- src/nav/MyNavDropdown.jsx
+++ src/nav/MyNavDropdown.jsx
@@ -4,37 +4,51 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 
+import GalleryHouse from "../galleryHouse/GalleryHouse";
+import GitClothes from "../gitClothes/GitClothes";
+import AboutMe from "../aboutMe/AboutMe";
+import Links from "../links/Links"
+
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import { NavHashLink } from "react-router-hash-link";
+
 const MyNavDropdown = () => {
   return (
     <div className="App">
-      <Navbar className="nav" expand="sm" bg="blue">
-        <Nav className="nav" fill variant="pills" activeKey={null}>
-          <NavDropdown title="Software Projects" id="nav-dropdown">
-            {/**Gallery House */}
-            <LinkContainer to="/galleryhouse">
-              <NavDropdown.Item
-                style={{ width: "5rem" }}
-                eventKey="1.1"
-                title="Item"
-                id="nav-dropdown-item"
-              >
-                Gallery House
-              </NavDropdown.Item>
-            </LinkContainer>
-  
-            {/**Git-clothes */}
-            <LinkContainer to="/gitclothes">
-              <NavDropdown.Item
-                eventKey="1.2"
-                title="Item"
-                id="nav-dropdown-item"
-              >
-                Git-clothes
-              </NavDropdown.Item>
-            </LinkContainer>
+      <Navbar className="nav" expand="sm" bg="light">
+        <Container fluid>
+          <Navbar.Brand href="#">Portfolio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="nav" fill variant="pills" activeKey={null}>
+              <NavDropdown title="Software Projects" id="nav-dropdown">
+                {/**Gallery House */}
+                <LinkContainer to="/galleryhouse">
+                  <NavDropdown.Item
+                    style={{ width: "5rem" }}
+                    eventKey="1.1"
+                    title="Item"
+                    id="nav-dropdown-item"
+                  >
+                    Gallery House
+                  </NavDropdown.Item>
+                </LinkContainer>
 
-            {/**My GitHub */}
-            {/* <NavDropdown.Item
+                {/**Git-clothes */}
+                <LinkContainer to="/gitclothes">
+                  <NavDropdown.Item
+                    eventKey="1.2"
+                    title="Item"
+                    id="nav-dropdown-item"
+                  >
+                    Git-clothes
+                  </NavDropdown.Item>
+                </LinkContainer>
+
+                {/**My GitHub */}
+                {/* <NavDropdown.Item
               eventKey="1.3"
               // onClick={() => alert(`Redirecting you to GitHub. Okay?`)}
               href="https://github.com/grace-sanford"
@@ -43,27 +57,39 @@ const MyNavDropdown = () => {
             >
               My GitHub
             </NavDropdown.Item> */}
-          </NavDropdown>
+              </NavDropdown>
 
-          <Nav.Item>
-            {/**About */}
-            <LinkContainer to="/about">
-              <Nav.Link eventKey="2" title="Item" id="nav-item">
-                About me
-              </Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
+              <Nav.Item>
+                {/**About */}
+                <Nav.Link
+                  eventKey="2"
+                  title="Item"
+                  id="nav-item"
+                  activeClassName="active-link" // <-- define active classname
+                  as={NavHashLink} // <-- render NavHashLink component
+                  className="style-navlink"
+                  smooth // <-- smooth scrolling
+                  to="#about"
+                >
+                  About me
+                </Nav.Link>
+              </Nav.Item>
 
-          {/**Contact me */}
-          <Nav.Item>
-            <LinkContainer to="/links">
-              <Nav.Link eventKey="3" id="nav-item">
-                Get in touch
-              </Nav.Link>
-            </LinkContainer>
-          </Nav.Item>
-        </Nav>
+              {/**Contact me */}
+              <Nav.Item>
+                <LinkContainer to="/links">
+                  <Nav.Link eventKey="3" id="nav-item">
+                    Get in touch
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
+
+      <GalleryHouse />
+      <AboutMe />
     </div>
   );
 };
